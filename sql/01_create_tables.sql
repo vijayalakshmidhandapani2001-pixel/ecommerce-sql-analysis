ddbaf7bb5d84a520b87fe375236355d06ee1c0a2
vijayalakshmidhandapani2001-pixel <vijayalakshmidhandapani2001@gmail.com>
2026-09-09 21:22:37 +0530

    Update 01_create_tables.sql

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -42,7 +42,7 @@ CREATE TABLE olist_order_payments_dataset (
     payment_installments INT,
     payment_value DECIMAL(10,2)
 );
-
+GO
 
 CREATE TABLE olist_products_dataset (
     product_id VARCHAR(50) PRIMARY KEY,
@@ -55,7 +55,7 @@ CREATE TABLE olist_products_dataset (
     product_height_cm INT,
     product_width_cm INT
 );
-
+GO
 
 CREATE TABLE olist_sellers_dataset (
     seller_id VARCHAR(50) PRIMARY KEY,
@@ -63,7 +63,7 @@ CREATE TABLE olist_sellers_dataset (
     seller_city VARCHAR(100),
     seller_state VARCHAR(5)
 );
-
+GO
 
 CREATE TABLE olist_geolocation_dataset (
     geolocation_zip_code_prefix VARCHAR(10),
@@ -72,9 +72,10 @@ CREATE TABLE olist_geolocation_dataset (
     geolocation_city VARCHAR(100),
     geolocation_state VARCHAR(5)
 );
-
+GO
 
 CREATE TABLE product_category_name_translation (
     product_category_name VARCHAR(100),
     product_category_name_english VARCHAR(100)
 );
+GO
